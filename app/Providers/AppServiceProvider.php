8556260b6b9d97fbfb0a6420d443d9994610f856
Fernando Castillo <fdocst@gmail.com>
2026-06-01 21:09:31 -0500

    fix(ui): registrar namespace de componentes para layouts y corregir sintaxis

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        \Illuminate\Support\Facades\Blade::anonymousComponentPath(resource_path('views/layouts'), 'layouts');
     }
 
     /**
