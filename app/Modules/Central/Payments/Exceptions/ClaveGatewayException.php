ba5f49b7a15bb2c829128c892c768ef02a6e820f
Fernando Castillo <ferncastillov@outlook.com>
2026-06-09 15:37:14 -0500

    refactor(payments): separar excepciones en archivos individuales PSR-4

---
+++ app/Modules/Central/Payments/Exceptions/ClaveGatewayException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Central\Payments\Exceptions;
+
+use RuntimeException;
+
+class ClaveGatewayException extends RuntimeException {
+}
---
+++ app/Modules/Central/Payments/Exceptions/InvalidMerchantException.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Central\Payments\Exceptions;
+
+final class InvalidMerchantException extends ClaveGatewayException {
+}
