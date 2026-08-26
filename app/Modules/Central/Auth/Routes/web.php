50cfaee734e06eed62ba4465d067e82c7faa3d0d
nandocdev <fdocst@gmail.com>
2026-08-25 21:43:56 -0500

    chore(php): declarar strict_types en toda la base de código

--- app/Modules/Central/Auth/Routes/web.php
+++ app/Modules/Central/Auth/Routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Modules\Central\Auth\Actions\LogoutCentralUserAction;
 use App\Modules\Central\Auth\Http\Middleware\ValidateCentralSession;
 use App\Modules\Central\Auth\Livewire\Dashboard;
--- app/Modules/Central/Billing/Database/Factories/PaymentFactory.php
+++ app/Modules/Central/Billing/Database/Factories/PaymentFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Modules\Central\Billing\Database\Factories;
 
 use App\Modules\Central\Billing\Domain\Models\Payment;
