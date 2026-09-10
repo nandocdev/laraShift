b50ee9e912a794526934ba7c88260156276f3ab6
nandocdev <fdocst@gmail.com>
2026-09-09 22:26:45 -0500

    chore(billing): remove Billing, Catalog, Metering and dLocal modules
    Delete Central/Billing, Central/Catalog, Platform/Integrations/Dlocal, Platform/Contracts/Billing, Platform/Metering, payment/subscription platform events, feature/quota middleware, SnapshotQuotasJob, billing/metering configs, plans and payment migrations, Plan/Feature seeders, billing test suites, clave JS adapter and dedicated Billing/Catalog docs.

--- app/Modules/Central/Billing/Domain/Exceptions/ClaveGatewayException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Modules\Central\Billing\Domain\Exceptions;
-
-use RuntimeException;
-
-class ClaveGatewayException extends RuntimeException {}
--- app/Modules/Platform/Integrations/Dlocal/Exceptions/DlocalSignatureException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Modules\Platform\Integrations\Dlocal\Exceptions;
-
-use RuntimeException;
-
-final class DlocalSignatureException extends RuntimeException {}
